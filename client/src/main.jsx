cef6811b79346592195551faaae811f34193da2c
Fakhri Miftahul Khairi <miftahulkhairifakhri@gmail.com>
2026-07-05 12:06:28 +0700

    fix: add basename to BrowserRouter for GitHub Pages compatibility

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ injectStore(store);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
         <Toaster
           position="top-right"
